546f678cd4a220989d3e237b1958e51efccadfa7
Wasabi <wasabilikeselmo@gmail.com>
2016-10-08 21:28:31 +1100

    Working example

--- examples/04_remote_controller/runner.py
+++ examples/04_remote_controller/runner.py
@@ -15,42 +15,39 @@
 # You should have received a copy of the GNU General Public License
 # along with Kitty.  If not, see <http://www.gnu.org/licenses/>.
 
-#'''
-#Usage:
-#    ./runner [--kitty-options=<kitty-options>]
-#Options:
-#    -k --kitty-options <kitty-options>  options for the kitty fuzzer, use --kitty-options=--help to get a full list
-#This example stores the mutations in files under ./tmp/
-#It also demonstrate how to user kitty fuzzer command line options.
-#'''
-
-import docopt
 from kitty.fuzzers import ServerFuzzer
 from kitty.interfaces import WebInterface
-from kitty.controllers import EmptyController
 from katnip.targets.file import FileTarget
 from kitty.model import GraphModel
 from kitty.model import String
 from kitty.model import Template
+from kitty.remote.actor import RemoteActor
 
-opts = docopt.docopt(__doc__)
+
 t1 = Template(name='T1', fields=[
-    String('The default string', name='S1_1'),
-    String('Another string', name='S1_2'),
-])
+        String('The default string', name='S1_1'),
+        ])
 
 # Writes content to files
 target = FileTarget('FileTarget', 'tmp/', 'fuzzed')
-controller = EmptyController('EmptyController')
+
+#
+# connects to actual actor (controller) over RPC
+#
+controller = RemoteActor('127.0.0.1', 25002)
 target.set_controller(controller)
 
 model = GraphModel()
 model.connect(t1)
 
-fuzzer = ServerFuzzer(name="Example 1 - File Generator", option_line=opts['--kitty-options'])
+fuzzer = ServerFuzzer(name='Example 4 - File Generator(Remote Controller)')
 fuzzer.set_interface(WebInterface(port=26001))
 fuzzer.set_model(model)
 fuzzer.set_target(target)
+# No need for delay, we only create files
+# fuzzer.set_delay_between_tests(0.02)
+# No need for range, generate all
+# fuzzer.set_range(50)
 fuzzer.start()
 print('-------------- done with fuzzing -----------------')
 raw_input('press enter to exit')
